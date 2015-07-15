5691cc6347261100df03651755c0ba0bb5c3768c
mbi <mbi@hacon.de>
2015-07-15 15:42:40 +0200

    introduced hasprefix/nothasprefix for if marker

--- MGTemplateStandardMarkers.m
+++ MGTemplateStandardMarkers.m
@@ -394,6 +394,22 @@
 							argTrue = !argTrue;
 						}
 					}
+                } else if ([op isEqualToString:@"hasprefix"] || [op isEqualToString:@"nothasprefix"]) {
+                    NSObject *firstVal = [engine resolveVariable:firstArg];
+                    NSObject *secondVal = [engine resolveVariable:secondArg];
+                    if (!firstVal) {
+                        firstVal = firstArg;
+                    }
+                    if (!secondVal) {
+                        secondVal = secondArg;
+                    }
+                    if (firstVal && secondVal) {
+                        //NSLog(@"%@ %@", [NSString stringWithFormat:@"%@", firstVal], [NSString stringWithFormat:@"%@", secondVal]);
+                        argTrue = [[NSString stringWithFormat:@"%@", firstVal] hasPrefix:[NSString stringWithFormat:@"%@", secondVal]];
+                        if ([op isEqualToString:@"nothasprefix"]) {
+                            argTrue = !argTrue;
+                        }
+                    }
 				}
 			}
 			
